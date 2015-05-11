0a5c47fb69316e66fde5950650b48257c32d664f
Conor Redmond <doubleirish@users.noreply.github.com>
2015-05-11 01:57:30 -0700

    add 2 @select labs

---
+++ src/main/java/edu/uw/data/lecture8/model/ProductLine2.java
@@ -0,0 +1,40 @@
+package edu.uw.data.lecture8.model;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Created by credmond on 26/03/15.
+ */
+@Entity
+@Table(name = "PRODUCT_LINES", schema = "APP", catalog = "")
+public class ProductLine2 {
+  private String productLine;
+  private String textDescription;
+
+
+  @Id
+  @Column(name = "PRODUCT_LINE")
+  public String getProductLine() {
+    return productLine;
+  }
+
+  public void setProductLine(String productLine) {
+    this.productLine = productLine;
+  }
+
+  @Basic
+  @Column(name = "TEXT_DESCRIPTION", nullable = true, insertable = true, updatable = true, length = 4000)
+  public String getTextDescription() {
+    return textDescription;
+  }
+
+  public void setTextDescription(String textDescription) {
+    this.textDescription = textDescription;
+  }
+
+
+}
